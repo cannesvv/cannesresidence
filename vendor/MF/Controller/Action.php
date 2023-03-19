f7e1ef561ebe2c9e23101de1126f078671936c04
cannesvv <cannes.residence.vv@gmail.com>
2023-03-19 08:48:24 -0300

    Atualiza caminho

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -28,7 +28,7 @@ abstract class Action {
 
 		$classAtual = strtolower(str_replace('Controller', '', $classAtual));
 
-		require_once "../App/Views/".$classAtual."/".$this->view->page.".phtml";
+		require_once "/App/Views/".$classAtual."/".$this->view->page.".phtml";
 	}
 }
 
