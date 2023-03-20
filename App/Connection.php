3b9ce95e840bb44b55578bfcabd543c94b9635b6
cannesvv <cannes.residence.vv@gmail.com>
2023-03-20 19:07:09 -0300

    Muda string de conexao com o banco

--- App/Connection.php
+++ App/Connection.php
@@ -17,10 +17,16 @@ class Connection {
 					array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") 
 				);
 			} else {
+				//$conn = new \PDO(
+				//	"mysql:host=localhost:3306;dbname=controle_de_acesso;charset=utf8",
+				//	"root",
+				//	"",
+				//	array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") 
+				//);
 				$conn = new \PDO(
-					"mysql:host=localhost:3306;dbname=controle_de_acesso;charset=utf8",
-					"root",
-					"",
+					"mysql:host=mysql.cannesresidence.com.br:3306;dbname=cannesresidenc;charset=utf8",
+					"cannesresi_add1",
+					"Rbt824655",
 					array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") 
 				);
 			} 
