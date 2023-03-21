3f1f067880105f3b4f031d1ca653573511e595ee
cannesvv <cannes.residence.vv@gmail.com>
2023-03-20 21:51:49 -0300

    Teste erro

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -28,8 +28,8 @@ abstract class Action {
 		//echo str_replace('Controller', '', $classAtual);
 		//exit;
 
-		//$classAtual = strtolower(str_replace('Controller', '', $classAtual));
-		$classAtual = str_replace('Controller', '', $classAtual);
+		$classAtual = strtolower(str_replace('Controller', '', $classAtual));
+		//$classAtual = str_replace('Controller', '', $classAtual);
 
 		require_once "../App/Views/".$classAtual."/".$this->view->page.".phtml";
 	}
